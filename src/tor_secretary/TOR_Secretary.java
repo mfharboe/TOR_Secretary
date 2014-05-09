58659b1aafcb5c5846795769db70b284e84d93fb
Morten <Morten@Morten-Pc>
2014-05-09 11:25:51 +0200

    Project created

---
+++ src/tor_secretary/TOR_Secretary.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tor_secretary;
+
+/**
+ *
+ * @author Morten
+ */
+public class TOR_Secretary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
